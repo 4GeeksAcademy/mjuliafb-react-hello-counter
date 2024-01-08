ce600d050055b0f12cb79ecc0d47c9bd30f43b06
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2024-01-08 18:11:04 +0000

    third commit buttons add it

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Home = (props) => {
+const Home = () => {
+  let isRunning = true; 
+  let intervalId; 
+
   const updateCounter = (counterRef) => {
     return () => {
+      if (!isRunning) return; 
       counterRef.current = (counterRef.current + 1) % 10000000;
       updateDOM();
     };
@@ -14,29 +18,65 @@ const Home = (props) => {
     const digitElements = document.querySelectorAll(".digit");
 
     digitElements.forEach((digitElement, index) => {
-      const digit = parseInt(contadorFormateado[index], 10) || 0;
+      const digit = parseInt(contadorFormateado[index]) || 0;
       digitElement.textContent = digit;
     });
   };
 
   const counterRef = { current: 0 };
 
-  setInterval(updateCounter(counterRef), 1000);
+  // Iniciar el intervalo
+  const startInterval = () => {
+    intervalId = setInterval(updateCounter(counterRef), 1000);
+  };
+
+
+  startInterval();
+
+  // Botón "Detener"
+  const handleStopClick = () => {
+    isRunning = false;
+    clearInterval(intervalId); 
+  };
+
+  // Botón "Reiniciar"
+  const handleRestartClick = () => {
+    isRunning = true;
+    clearInterval(intervalId);
+    startInterval(); 
+  };
+
+  // Botón "Limpiar"
+  const handleClearClick = () => {
+    isRunning = true;
+    counterRef.current = 0;
+    clearInterval(intervalId); 
+    startInterval(); 
+    updateDOM(); 
+  };
 
   return (
-    <div className="counter d-flex justify-content-center">
-      <div className="clock">
-        <FontAwesomeIcon icon="fa-regular fa-clock" />
-      </div>
-      {[...Array(7)].map((_, index) => (
-        <div key={index} className={`digit digit${index + 1}`}>
-          0
+    <div className="bigDiv text-center">
+      <div className="counter d-flex justify-content-center">
+        <div className="clock">
+          <FontAwesomeIcon icon="fa-regular fa-clock" />
         </div>
-      ))}
+        {[...Array(7)].map((_, index) => (
+          <div key={index} className={`digit digit${index + 1}`}>
+            0
+          </div>
+        ))}
+      </div>
+      <button className="stopButton m-3" onClick={handleStopClick}>Pausar</button>
+      <button className="restartButton m-3" onClick={handleRestartClick}>Seguir</button>
+      <button className="clearButton m-3" onClick={handleClearClick}>Reiniciar</button>
     </div>
   );
 };
 
 export default Home;
+
+
+
 
 
